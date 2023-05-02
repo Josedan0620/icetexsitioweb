3fb94cc305aebc8049a4ad99a201024f6441be09
Josedan0620 <74471497+Josedan0620@users.noreply.github.com>
2023-05-02 15:04:36 -0500

    Update appFlask.py

--- appFlask.py
+++ appFlask.py
@@ -1,26 +1,41 @@
-import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+import sklearn 
 
-def main():
-    st.title("Aplicación de ejemplo")
-    st.write("¡Hola, bienvenido a Streamlit!")
+from flask import Flask,render_template,request
+app=Flask(name)
 
-    # Agregar elementos interactivos
-    nombre = st.text_input("Ingresa tu nombre")
-    st.write(f"Hola {nombre}!")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    edad = st.slider("Ingresa tu edad", 0, 100, 25)
-    st.write(f"Tienes {edad} años")
+@app.route ('/prediccion', methods=['GET','POST'])
+def predict():
+    if request.method =='POST':
+        try:
+            var_1=float(request.form['var_1'])
+            var_2=float(request.form['var_2'])
 
-    colores = ["Rojo", "Verde", "Azul"]
-    color_seleccionado = st.selectbox("Elige un color", colores)
-    st.write(f"Has seleccionado el color {color_seleccionado}")
+            pred_args=[var_1,var_2]
+            pred_arr=np.array(pred_args)
+            preds=pred_arr.reshape(1,-1)
+            modelo=open("./modelo.pkl","rb")
+            modelo_clas=joblib.load(modelo)
+            prediccion_modelo=modelo_clas.predict(preds)
+            prediccion_modelo=round (float(prediccion_modelo),2)
+            if prediccion_modelo == 1.0:
+                 prediccion_modelo = "Aprueba"
+            else:
+                 prediccion_modelo = "No aprueba"
+        except ValueError:
+            return "Por favor entra nombre válidos"
+        return render_template('prediccion.html',prediccion=prediccion_modelo)
 
-    if st.button("¡Haz clic aquí!"):
-        st.write("¡Botón presionado!")
 
-if __name__ == "__main__":
-    main()
 
+if name=='main':
+    app.run(debug=True)
 
 
 
